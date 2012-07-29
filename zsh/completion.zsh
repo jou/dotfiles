7fe2be4a0fdd5e9e4a14811a10aa6543fb778617
Jiayong Ou <jou@orly.ch>
2012-07-29 22:57:42 +0200

    Completion config

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,12 @@
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+
 # matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|[._-]=** r:|=**'
+zstyle ':completion:*' menu select=1
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+zstyle :compinstall filename $ZSH/zsh/completion.zsh
