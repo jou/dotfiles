c3d875fe6ecf5d95779a45d04270735c66089b07
Jiayong Ou <jou@orly.ch>
2014-01-16 13:05:55 +0100

    small change to git/completion.sh

--- git/completion.sh
+++ git/completion.sh
@@ -1,9 +1,10 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+git_completion=/usr/local/etc/bash_completion.d/git-completion.bash
 
-if ! type _git >/dev/null 2>/dev/null && test -f $completion
+if ! type _git >/dev/null 2>/dev/null && test -f $git_completion
 then
-  source $completion
+  source $git_completion
 fi
+
